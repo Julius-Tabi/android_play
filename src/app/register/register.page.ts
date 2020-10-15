22cf32971e86e83fe03233652f4c635966932980
Julius-Tabi <67975193+Julius-Tabi@users.noreply.github.com>
2020-10-15 06:51:51 +0200

    secondScreen

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  showpassword = false;
+  passwordToggleIcon = 'eye';
   constructor() { }
-
+  togglePassword() {
+    this.showpassword = !this.showpassword;
+  }
   ngOnInit() {
   }
 
